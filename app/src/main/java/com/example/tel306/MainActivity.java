c52320ca707be9f88b3b45efcd56bcb4a83b63e7
Alvaro Burga <a20150632@pucp.pe>
2020-10-13 09:55:20 -0500

    aumento

--- app/src/main/java/com/example/tel306/MainActivity.java
+++ app/src/main/java/com/example/tel306/MainActivity.java
@@ -18,15 +18,16 @@ public class MainActivity extends AppCompatActivity {
             "La perserverancia es el camino al exito"};
     String [] palabras_relajantes = {"Un descanso de mas energia", "Suficiente por ahora, toma agua",
             "para un segundo para volver con más ganas"};
-    int ciclo;
+    int ciclo=1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final ContadorViewModel contadorViewModel=new ViewModelProvider(this).get(ContadorViewModel.class);
-        ImageView seting = (ImageView) findViewById(R.id.set);
 
+        ImageView seting = (ImageView) findViewById(R.id.set);
+        final ContadorViewModel contadorViewModel=new ViewModelProvider(this).get(ContadorViewModel.class);
         contadorViewModel.getTrabajo().observe(this, new Observer<Integer>() {
             @Override
             public void onChanged(Integer integer) {
@@ -45,6 +46,59 @@ public class MainActivity extends AppCompatActivity {
 
                 TextView temporizador = findViewById(R.id.tiempo_trabajo);
                 temporizador.setText(min + ":" + seg);
+                if (integer==fin)
+                {
+                    contadorViewModel.cuentaDescanso();
+                    int n=palabras_relajantes.length;
+                    Random random = new Random();
+                    int index =  random.nextInt(n);
+                    String palabra = palabras_relajantes[index];
+                    TextView textView = findViewById(R.id.mensajes);
+                    textView.setText(palabra);
+                }
+            }
+        });
+
+        contadorViewModel.getDescanso().observe(this, new Observer<Integer>() {
+            @Override
+            public void onChanged(Integer integer) {
+                int fin = contadorViewModel.getFinDescanso();
+                String min= String.valueOf ((fin-integer)/60);
+                int segi=(fin-integer)%60;
+                String seg;
+                if(segi<10)
+                {
+                    seg = "0"+String.valueOf(segi);
+                }
+                else
+                {
+                    seg = String.valueOf(segi);
+                }
+
+                TextView temporizador = findViewById(R.id.tiempo_descanso);
+                temporizador.setText(min + ":" + seg);
+                if(integer==fin)
+                {
+                    ciclo++;
+                    TextView tv = findViewById(R.id.ciclo_pomodoro);
+                    tv.setText("ciclo pomodoro "+ String.valueOf(ciclo) +" de 4");
+                    if (ciclo<4)
+                    {
+                        contadorViewModel.getDescanso().setValue(0);
+                        contadorViewModel.getTrabajo().setValue(0);
+                        contadorViewModel.cuentaTrabajo();
+                        int n=palabras_concentrantes.length;
+                        Random random = new Random();
+                        int index =  random.nextInt(n);
+                        String palabra = palabras_concentrantes[index];
+                        TextView textView = findViewById(R.id.mensajes);
+                        textView.setText(palabra);
+                    }
+                    else
+                    {
+                        //fin del programa
+                    }
+                }
             }
         });
 
@@ -54,14 +108,29 @@ public class MainActivity extends AppCompatActivity {
                 ImageView iv = (ImageView) v;
                 if (contadorViewModel.getHilo()==null)
                 {
-                    contadorViewModel.cuentaTrabajo();
                     iv.setImageResource(R.drawable.pause);
-                    int n=palabras_concentrantes.length;
-                    Random random = new Random();
-                    int index =  random.nextInt(n);
-                    String palabra = palabras_concentrantes[index];
-                    TextView textView = findViewById(R.id.mensajes);
-                    textView.setText(palabra);
+                    if (contadorViewModel.getActivo().equalsIgnoreCase("trabajo"))
+                    {
+                        contadorViewModel.cuentaTrabajo();
+                        int n=palabras_concentrantes.length;
+                        Random random = new Random();
+                        int index =  random.nextInt(n);
+                        String palabra = palabras_concentrantes[index];
+                        TextView textView = findViewById(R.id.mensajes);
+                        textView.setText(palabra);
+                    }
+                    else
+                    {
+                        contadorViewModel.cuentaDescanso();
+                        int n=palabras_relajantes.length;
+                        Random random = new Random();
+                        int index =  random.nextInt(n);
+                        String palabra = palabras_relajantes[index];
+                        TextView textView = findViewById(R.id.mensajes);
+                        textView.setText(palabra);
+                    }
+
+
                 }
                 else
                 {
@@ -77,14 +146,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 contadorViewModel.detenerContador();
+                contadorViewModel.setActivo("trabajo");
                 ImageView iv = findViewById(R.id.set);
                 iv.setImageResource(R.drawable.play);
                 contadorViewModel.getTrabajo().setValue(0);
+                contadorViewModel.getDescanso().setValue(0);
                 TextView textView = findViewById(R.id.mensajes);
                 textView.setText(" ");
+                ciclo=1;
             }
         });
-
-
     }
 }
