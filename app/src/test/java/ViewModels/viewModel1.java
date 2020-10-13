17b5f739abaaa62b3ff0130a7a57c41ea2c2f8d2
Alvaro Burga <a20150632@pucp.pe>
2020-10-12 23:21:23 -0500

    hilos

--- app/src/test/java/ViewModels/viewModel1.java
+++ app/src/test/java/ViewModels/viewModel1.java
@@ -1,7 +1,62 @@
 package ViewModels;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class viewModel1 extends ViewModel {
-    
+
+
+    Thread hilo;
+    MutableLiveData<Integer> trabajo = new MutableLiveData<>();
+    MutableLiveData<Integer> descanso = new MutableLiveData<>();
+    int finTrabajo=25*60;
+    int finDescanso=5*60;
+
+    public void cuentaTrabajo()
+    {
+
+        final int tiempoFin = finTrabajo;
+        hilo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int contadorLocal=0;
+                while (contadorLocal<tiempoFin)
+                {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    contadorLocal++;
+                    trabajo.postValue(contadorLocal);
+                }
+            }
+        });
+        hilo.start();
+    }
+
+    public void cuentaDescanso()
+    {
+
+        final int tiempoFin = finDescanso;
+        hilo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int contadorLocal=0;
+                while (contadorLocal<tiempoFin)
+                {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    contadorLocal++;
+                    descanso.postValue(contadorLocal);
+                }
+            }
+        });
+        hilo.start();
+    }
+
+
 }
